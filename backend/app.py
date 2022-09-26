bee581d1b2d77914420976ec096d426b5feba1ec
Rijikashi <isaacjfu@gmail.com>
2022-09-26 04:57:29 -0700

    frontend - initialized sample project. backend - added mongoDB and API call to return all decks

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from bson import json_util
+from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
+client = MongoClient('localhost',27017)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+db = client.flask_db
+decks = db.decks
+# def init_db():
+#     db = 
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+    
+@app.route('/getDecks')
+def getDecks():
+    x = parse_json(decks.find())
+    return jsonify(x)
+
+@app.route('/makeDeck', methods=('GET','POST') )
+def makeDeck():
+    if request.method=='POST':
+        name = request.form['name']
+        description = request.form['description']
+        phrases = request.form['phrases']
+        decks.insert_one({'name': name, 'description': description,'phrases': phrases})
+        return redirect(url_for('makeDeck'))
+    return render_template('index.html')
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
